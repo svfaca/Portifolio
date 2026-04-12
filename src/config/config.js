d782c7bc425e2d4c98ffe9dc06aae16835d9901b
Sávio Emmanuel <savioemmanuelsc@gmail.com>
2026-04-12 02:50:32 -0300

    atualização

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 // Configurações padrão do portfólio
 export const defaultConfig = {
   nome: "Sávio Emmanuel",
-  titulo: "Desenvolvedor Full Stack Júnior | IA | Dados | Automação",
+  titulo: "Desenvolvedor de IA Aplicada | Sistemas com LLMs e Automação",
   sobre_mim: "Desenvolvedor de Software",
   email: "savioemmanuelsc@gmail.com",
   telefone: "(21) 99784-3300",
